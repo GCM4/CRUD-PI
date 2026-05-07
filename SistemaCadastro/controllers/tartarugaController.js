d874c2300ca3d339b6c83cd357ac765928dd99cf
GCM4 <guiguicarneiro04@gmail.com>
2026-05-07 13:34:56 -0300

    Initial commit

---
+++ SistemaCadastro/controllers/tartarugaController.js
@@ -0,0 +1,62 @@
+import Tartaruga from '../models/Tartaruga.js'
+
+const TartarugaController = {
+
+  async listar(req, res) {
+
+    const tartarugas = await Tartaruga.findAll({
+      order: [['id', 'DESC']]
+    })
+
+    res.render('index', { tartarugas })
+  },
+
+
+  cadastro(req, res) {
+    res.render('cadastro')
+  },
+
+
+  async salvar(req, res) {
+
+    const {
+      identificacao,
+      especie,
+      local,
+      situacao,
+      observacoes
+    } = req.body
+
+    let imagem = ''
+
+    if (req.file) {
+      imagem = req.file.filename
+    }
+
+    await Tartaruga.create({
+      identificacao,
+      especie,
+      local,
+      situacao,
+      observacoes,
+      imagem
+    })
+
+    res.redirect('/')
+  },
+
+
+  async excluir(req, res) {
+
+    const { id } = req.params
+
+    await Tartaruga.destroy({
+      where: { id }
+    })
+
+    res.redirect('/')
+  }
+
+}
+
+export default TartarugaController
---
+++ SistemaCadastro/server.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import path from 'path'
+import multer from 'multer'
+import { fileURLToPath } from 'url'
+
+import sequelize from './models/database.js'
+import TartarugaController from './controllers/tartarugaController.js'
+
+const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs')
+
+const storage = multer.diskStorage({
+
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads')
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+
+})
+
+const upload = multer({ storage })
+
+app.get('/', TartarugaController.listar)
+
+app.get('/cadastro', TartarugaController.cadastro)
+
+app.post(
+  '/salvar',
+  upload.single('imagem'),
+  TartarugaController.salvar
+)
+
+app.post('/excluir/:id', TartarugaController.excluir)
+
+sequelize.sync()
+  .then(() => {
+
+    app.listen(3000, () => {
+      console.log('Servidor rodando em http://localhost:3000')
+    })
+
+  })
+  .catch(err => {
+    console.log(err)
+  })
